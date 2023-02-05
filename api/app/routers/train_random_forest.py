f4683a6f2241ca468945e3ef4af15e91910973d2
Thymen van de Lagemaat <thymenyj@gmail.com>
2023-02-05 16:53:12 +0100

    model training endpoint

--- api/app/routers/train_random_forest.py
+++ api/app/routers/train_random_forest.py
@@ -13,13 +13,12 @@ async def train_random_forest():
     try:
         connection_string = os.getenv("ANALYTICS_DATABASE_CONNECTION_STRING")
         analytics_database = AnalyticsDatabase(connection_string)
-        
+
         data_processor = RandomForestTrainer(analytics_database)
         data_processor.train()
 
-        
     except Exception as exc:
         print(exc)
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR) from exc
 
-    return 
+    return
--- api/app/src/model_trainers/RandomForestTrainer.py
+++ api/app/src/model_trainers/RandomForestTrainer.py
@@ -1,7 +1,9 @@
 from sqlalchemy import text
 from pandas import DataFrame
-
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+        
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
 class RandomForestTrainer:
     
     def __init__(self, analytics_database):
@@ -9,8 +11,27 @@ class RandomForestTrainer:
         
     def train(self):
         visitors_dataframe = self._get_visitors_dataframe()
-        print(visitors_dataframe.iloc[:10])
-    
+        X_train, X_test, y_train, y_test = train_test_split(
+            visitors_dataframe.drop(columns=["positive_revenue"]),
+            visitors_dataframe["positive_revenue"],
+            test_size = 0.2,
+            random_state=1
+        )
+
+        forest = RandomForestClassifier(n_estimators=100, n_jobs=20, random_state=2, verbose=True)
+        
+        print("Start training random forest...")
+        forest.fit(X_train, y_train.values.ravel())
+        
+        print("Evaluating random forest...")
+        prediction = forest.predict(X_test)
+        matrix = confusion_matrix(y_test, prediction)
+        accuracy = accuracy_score(y_test, prediction)
+        f1 = f1_score(y_test, prediction)
+        print(f"accuracy: {accuracy}")
+        print(f"f1_score: {f1}")
+        print(matrix)
+
     def _get_visitors_dataframe(self):
         try:
             sql_query = text("SELECT * FROM visitors")
